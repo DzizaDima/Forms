3e813eb7c351d3536b7ea8935618457540c906bb
DzizaDima <32745918+DzizaDima@users.noreply.github.com>
2020-11-15 11:36:36 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,54 @@
+
+function testWebP(callback) {
+
+  var webP = new Image();
+  webP.onload = webP.onerror = function () {
+    callback(webP.height == 2);
+  };
+  webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+}
+
+testWebP(function (support) {
+
+  if (support == true) {
+    document.querySelector('body').classList.add('webp');
+  } else {
+    document.querySelector('body').classList.add('no-webp');
+  }
+});
+
+
+
+var canvas = document.getElementById('signature-pad');
+
+
+function resizeCanvas() {
+
+  var ratio = Math.max(window.devicePixelRatio || 1, 1);
+  canvas.width = canvas.offsetWidth * ratio;
+  canvas.height = canvas.offsetHeight * ratio;
+  canvas.getContext("2d").scale(ratio, ratio);
+}
+
+window.onresize = resizeCanvas;
+resizeCanvas();
+
+var signaturePad = new SignaturePad(canvas, {
+  backgroundColor: 'rgb(255, 255, 255)'
+});
+
+
+document.getElementById('undo').addEventListener('click', function () {
+  var data = signaturePad.toData();
+  if (data) {
+    data.pop();
+    signaturePad.fromData(data);
+  }
+});
+
+
+
+
+$(document).ready(function () {
+  $('.toast').toast('show');
+});
