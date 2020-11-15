3ec96e85e8c8929ac96739d5189703fbb9ae9862
DzizaDima <32745918+DzizaDima@users.noreply.github.com>
2020-11-15 12:49:10 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -26,7 +26,7 @@ function resizeCanvas() {
 
   var ratio = Math.max(window.devicePixelRatio || 1, 1);
   canvas.width = canvas.offsetWidth * ratio;
-  canvas.height = canvas.offsetHeight * ratio;
+/*   canvas.height = canvas.offsetHeight * ratio; */
   canvas.getContext("2d").scale(ratio, ratio);
 }
 
